0d16b163aea205f656c14fd6f01482ed06a9e0d7
Maria Eduarda Ribeiro Facio <duda.ribeirof07@gmail.com>
2022-07-19 19:54:40 -0300

    ajustes na classe no

--- include/No.h
+++ include/No.h
@@ -1,21 +1,11 @@
-/*******************************************************************
- *   Defines
- *******************************************************************/
 #ifndef NO_H
 #define NO_H
 
-/*******************************************************************
- *   Includes
- *******************************************************************/
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include "aresta.h"
-
-/*******************************************************************
- *   DECLARAÇÕES DOS METODOS
- *******************************************************************/
 
 using namespace std;
 
@@ -24,20 +14,20 @@ class No
 {
 
 private:
-    int id;       // id contido em um nó.
+    int id;       // ID contido em um nó.
     Aresta *prox; // Aresta que liga o nó atual aos seus adjacentes.
     int pesoNo;   // Peso do respectivo nó
 
 public:
-    No(int id);         // Construtor da classe Nó.
-    ~No();              // Destrutor da classe Nó.
-    int getId();        // Retorna o ID do nó.
-    void setId(int id); // Altera o ID do nó.
-    int getPesoNo();            //Retorna o peso do nó.
-    void setPesoNo(int peso);   //Altera o peso do nó.
-    Aresta *getProx();              // Retorna o ponteiro da aresta que liga o nó ao adjacente.
-    void setProx(Aresta *prox);     // Altera o nó adjacente.
-    void adcAresta(No *n, int peso);// adiciona uma aresta na lista de arestas
+    No(int id);                         // Construtor da classe Nó.
+    ~No();                              // Destrutor da classe Nó.
+    int getId();                        // Retorna o ID do nó.
+    void setId(int id);                 // Altera o ID do nó.
+    int getPesoNo();                    // Retorna o peso do nó.
+    void setPesoNo(int peso);           // Altera o peso do nó.
+    Aresta *getProx();                  // Retorna o ponteiro da aresta que liga o nó ao adjacente.
+    void setProx(Aresta *prox);         // Altera o nó adjacente.
+    void adcAresta(No *n, int peso);    // Adiciona uma aresta na lista de arestas
 };
 
 #endif
--- src/No.cpp
+++ src/No.cpp
@@ -1,25 +1,13 @@
-/*******************************************************************
- *   Includes
- *******************************************************************/
-
 #include "no.h"
 
-/*******************************************************************
- *   IMPLEMENTAÇÃO
- *******************************************************************/
-
-/**
- * Identificação do no e inicialização do no
- */
+// Identificação do no e inicialização do no
 No::No(int id)
 {
     this->id = id;
     this->prox = NULL;
 }
 
-/**
- * Destrutor do objeto No
- */
+// Destrutor do objeto No
 No::~No()
 {
     if (prox != NULL)
@@ -28,57 +16,43 @@ No::~No()
     }
 }
 
-/**
- * Retorna o id do nó atual
- */
+// Retorna o id do nó atual
 int No::getId()
 {
     return this->id;
 }
 
-/**
- * Modifica o id do nó atual
- */
+// Modifica o id do nó atual
 void No::setId(int id)
 {
     this->id = id;
 }
 
-/**
- * Determina o peso do nó atual
- */
+// Determina o peso do nó atual
 void No::setPesoNo(int peso)
 {
     this->pesoNo = peso;
 }
 
-/**
- * Retorna o peso do nó atual
- */
+// Retorna o peso do nó atual
 int No::getPesoNo()
 {
     return this->pesoNo;
 }
 
-/**
- * Retorna o proximo nó da lista encadeada de nós
- */
+// Retorna o proximo nó da lista encadeada de nós
 Aresta *No::getProx()
 {
     return this->prox;
 }
 
-/**
- * Determina o proximo nó da lista encadeadad de nós
- */
+// Determina o proximo nó da lista encadeadad de nós
 void No::setProx(Aresta *prox)
 {
     this->prox = prox;
 }
 
-/**
- * Adiciona aresta na lista encadeada das arestas
- */
+// Adiciona aresta na lista encadeada das arestas
 void No::adcAresta(No *n, int peso)
 {
     Aresta *a = new Aresta(n, this->getProx(), peso);
